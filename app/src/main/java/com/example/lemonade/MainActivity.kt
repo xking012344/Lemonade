f75a359935b09cb15ffbd944dc7e68412c421970
hardixav000 <hardixav000@warren.k12.in.us>
2024-09-23 12:10:27 -0400

    ...

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -7,13 +7,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -26,6 +30,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -86,7 +91,8 @@ fun LemonApp() {
                     Image(
                         painter = painterResource(R.drawable.lemon_squeeze),
                         contentDescription = stringResource(R.string.lemon),
-                        modifier = Modifier.wrapContentSize()
+                        modifier = Modifier
+                            .wrapContentSize()
                             .clickable {
                                 currentStep = 3
                             }
@@ -103,7 +109,8 @@ fun LemonApp() {
                     Spacer(modifier = Modifier.height(32.dp))
                     Image(painter = painterResource(id = R.drawable.lemon_drink),
                         contentDescription = stringResource(id = R.string.lemon_drink),
-                        modifier = Modifier.wrapContentSize()
+                        modifier = Modifier
+                            .wrapContentSize()
                             .clickable { currentStep = 4 }
                     )
                 }
@@ -118,7 +125,8 @@ fun LemonApp() {
                    Spacer(modifier = Modifier.height(32.dp))
                    Image(painter = painterResource(id = R.drawable.lemon_restart),
                        contentDescription = stringResource(id = R.string.empty_glass),
-                       modifier = Modifier.wrapContentSize()
+                       modifier = Modifier
+                           .wrapContentSize()
                            .clickable { currentStep = 1 })
 
                }
@@ -127,6 +135,40 @@ fun LemonApp() {
     }
 }
 
+@Composable
+fun LemonTextAndImage(
+    textLabelResourceId: Int,
+    drawableResourceId: Int,
+    contentDescriptionResourceId: Int,
+    onImageClick : () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Box(
+        modifier = modifier
+    ){
+       Column(
+           horizontalAlignment = Alignment.CenterHorizontally,
+           verticalArrangement = Arrangement.Center,
+           modifier = Modifier.fillMaxSize()
+       ){
+           Button(onClick = onImageClick,
+               shape = RoundedCornerShape(dimensionResource(id = R.dimen.corner_radius))     ,
+               colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.tertiaryContainer)
+           ) {
+               Image(painter = painterResource(id = drawableResourceId),
+                   contentDescription = stringResource(id = contentDescriptionResourceId),
+                   modifier = Modifier
+                       .width(dimensionResource(id = R.dimen.button_image_width))
+                       .height(dimensionResource(id = R.dimen.button_image_height))
+                       .padding(dimensionResource(id = R.dimen.button_interior_padding))
+               )
+           }
+           Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.padding_vertical)))
+           Text(text = stringResource(id = textLabelResourceId),
+               style = MaterialTheme.typography.bodyLarge)
+       }
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
